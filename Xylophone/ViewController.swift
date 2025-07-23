e5471119313c45da65c84fe1df28cc006de5baec
weldon <weldonyangg@gmail.com>
2025-07-23 13:50:21 -0700

    added sounds + opacity change on button press

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer!
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
+    func playNote(note: String) {
+        guard let url = Bundle.main.url(forResource: note, withExtension: "wav") else {
+            print("Could not find file")
+            return}
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer.play()
+        } catch {
+            print("Could not play file")
+        }
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playNote(note: sender.currentTitle!)
+        
+        sender.alpha = 0.5
 
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+    }
 }
 
